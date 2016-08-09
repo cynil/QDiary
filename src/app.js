49e4f47e2a2623a1852b2d9e4970ebe36ed02750
lianjian zhou <lianjian zhou>
2016-08-09 18:35:48 +0800

    total rewrite

--- src/app.js
+++ src/app.js
@@ -1,128 +1,182 @@
-var ngDiary = angular.module('ngDiary', ['ngRoute', 'simpleStorage'])
-ngDiary
+var QDiary = angular.module('QDiary', ['ngRoute', 'myFilters', 'myServices', 'simpleStorage'])
+
+QDiary
     .config(function DiaryRouteConfig($routeProvider){
         $routeProvider
             .when('/', {
                 controller: 'MainController',
-                templateUrl: 'templates/add.tmpl.html'
+                templateUrl: 'templates/default.tmpl.html'
             })
             .when('/view/:guid', {
                 controller: 'DetailController',
-                templateUrl: 'templates/view.tmpl.html'
+                templateUrl: 'templates/detail.tmpl.html'
             })
             .when('/edit/:guid',{
                 controller: 'EditController',
-                templateUrl: 'templates/new.tmpl.html'
+                templateUrl: 'templates/edit.tmpl.html'
             })
             .when('/new/', {
                 controller: 'NewController',
-                templateUrl: 'templates/new.tmpl.html'
+                templateUrl: 'templates/edit.tmpl.html'
             })
             .otherwise({
                 redirectTo: '/'
             })
     })
 
-    .controller('MainController', function($scope, $rootScope, $Storage$){
-
-        var DiaryService = $Storage$.open('diaries')
+    .run(function($rootScope, $location, DiaryService, $getItemByGuid){
 
         $rootScope.diaries = DiaryService.query()
 
-        if(!$Storage$.simple('used')){
-            var date = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('/')
+        $rootScope.$on('$routeChangeSuccess', function(evt){
+            var bread = $location.url().split('/').filter(function(v){
+                return v !== ''
+            })
+
+            if(bread[1]){
+                bread[1] = $rootScope.diaries[$getItemByGuid(bread[1], $rootScope.diaries)]['title']
+            }
+
+            $rootScope.breadcrumb = ['Home'].concat(bread)
+        })
+
+    })
+
+    .controller('MainController', function($rootScope, $myStorage, DiaryService, $location){
+
+        if(!$myStorage.simple('used')){
+            var date = new Date().toLocaleString();
             var welcome = {
                 title: 'Welcome!',
                 date: date,
-                content: 'Thank you for using ngDiary!'
+                contents: [
+                    'Thank you for using QDiary!',
+                    'QDiary is a lightweight diary web application build with AngularJS and Bootstrap.',
+                    'It is very safe and convenient, As it is backend-less -- all of its data are stored in your own computer\'s localStorage, and it is available event when youare offline',
+                    'Enjoy your time with writing diaries!'
+                    ]
             }
 
-            welcome.guid = DiaryService.create(welcome)
+            welcome.guid = DiaryService.save(welcome)
 
             $rootScope.diaries.push(welcome)
 
-            $Storage$.simple('used', 'used')
+            $myStorage.simple('used', 'used')
 
-        }
+            $location.path('/view/' + welcome.guid)
 
-        $rootScope.delete = function(guid){
-            var key
-
-            $rootScope.diaries.map(function(v, i, arr){
-                if(v.guid === guid){
-                    key =  i
-                }
-            })
-
-            DiaryService.delete($rootScope.diaries[key])
-            $rootScope.diaries.splice(key, 1)
         }
 
     })
-    
-    .controller('DetailController', function($scope, $rootScope, $routeParams){
-        var key
 
-        $rootScope.diaries.map(function(v, i, arr){
-            if(v.guid === $routeParams.guid){
-                key =  i
-            }
-        })
+    .controller('DetailController', function($scope, $rootScope, $routeParams, $location, $getItemByGuid, DiaryService){
 
+        var key = $getItemByGuid($routeParams.guid, $rootScope.diaries)
         $scope.diary = $rootScope.diaries[key]
 
+        $scope['delete'] = function(){
+            $rootScope.diaries.splice(key, 1)
+            DiaryService['delete']($scope.diary)
+
+            var next = $rootScope.diaries[key] ? $rootScope.diaries[key] : 0
+
+            if(next){
+                $location.path('/view/' + next.guid)
+            }else{
+                $location.path('/')
+            }
+        }
+        
     })
 
-    .controller('NewController', function($scope, $rootScope, $routeParams, $Storage$, $location){
+    .controller('EditController', function($scope, $rootScope, $routeParams, $location, $getItemByGuid, DiaryService){
 
-        var DiaryService = $Storage$.open('diaries')
+        $scope.diary = $rootScope.diaries[$getItemByGuid($routeParams.guid, $rootScope.diaries)]
+        $scope.rawContents = $scope.diary.contents.join('\n')
 
         $scope.saveItem = function(){
-            var date = [new Date().getFullYear(), new Date().getMonth() + 1, new Date().getDate()].join('/')
+            var item = {
+                title: $scope.diary.title,
+                date: $scope.diary.date,
+                contents: $scope.rawContents.split('\n'),
+                guid: $scope.diary.guid
+            }
+
+            DiaryService.save(item)
+            
+            $rootScope.diaries[$getItemByGuid($routeParams.guid, $rootScope.diaries)] = item
+
+            $location.path('/view/' + item.guid)
+        }
+    })
+
+    .controller('NewController', function($scope, $rootScope, $location, DiaryService){
+        $scope.rawContents = ''
+        $scope.saveItem = function(){
+            var date = new Date().toLocaleDateString()
             var item = {
                 title: $scope.diary.title,
                 date: date,
-                content: $scope.diary.content
+                contents: $scope.rawContents.split('\n')
             }
 
-            item.guid = DiaryService.create(item)
+            item.guid = DiaryService.save(item)
 
             $rootScope.diaries.push(item)
 
             $location.path('/view/' + item.guid)
         }
-
     })
-    .controller('EditController', function($scope, $rootScope, $routeParams, $Storage$, $location){
 
-        var key,
-            DiaryService = $Storage$.open('diaries')
 
-        $rootScope.diaries.map(function(v, i, arr){
-            if(v.guid === $routeParams.guid){
-                key =  i
-            }
-        })
-
-        $scope.diary = $rootScope.diaries[key]
-
-        $scope.saveItem = function(){
-
-            $rootScope.diaries[key] = $scope.diary;
-
-            DiaryService.update($scope.diary);
-
-            $location.path('/view/' + $scope.diary.guid)
+/*
+ *
+ * Filters
+ * 
+ */
+angular.module('myFilters', [])
+    .filter('hideExtra', function(){
+        return function(value, limit){
+            return value.length>limit ? value.substr(0, limit) + '...' : value
         }
     })
 
+/*
+ *
+ * Services
+ * 
+ */
+angular.module('myServices', ['simpleStorage'])
 
+    .factory('$getItemByGuid', function(){
+        return function(key, list){
+            for(var i = 0; i < list.length; i++){
+                if(list[i]['guid'] === key){
+                    return i
+                }
+            }
+        }
+    })
 
+    .factory('DiaryService', function($myStorage){
 
+        var db = $myStorage.open('diaries')
 
+        return {
+            query: function(){
+                return db.query()
+            },
 
+            save: function(item){
+                if(!item.guid || item.guid.indexOf('diaries') < 0){
+                    return db.create(item)
+                }else{
+                    return db.update(item)
+                }
+            },
 
-
-
-
-
+            'delete': function(item){
+                return db['delete'](item)
+            }
+        }
+    })
